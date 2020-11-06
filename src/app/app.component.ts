1b9fc845e4416504cedcdee80dbaf4a9ece232f4
Blessing <bntoko@gmail.com>
2020-11-07 00:50:22 +0200

    can randomly generate cubes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
   public controls = new OrbitControls(this.camera, this.renderer.domElement);
   public raycaster = new THREE.Raycaster();
   public mouse = new THREE.Vector2();
-  public sphereGeometry = new THREE.SphereGeometry(1, 40, 40);
+  public sphereGeometry = new THREE.SphereGeometry(1, 35, 35)
   public coneBuffGeometry = new THREE.ConeBufferGeometry(1, 2, 16);
   public boxGeometry = new THREE.BoxGeometry(2, 2, 2);
   public dodecGeometry = new THREE.DodecahedronBufferGeometry(2);
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
   }
 
   public init() {
-    this.renderer.setClearColor('#222');
+    this.renderer.setClearColor('#555');
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
@@ -38,10 +38,11 @@ export class AppComponent implements OnInit {
     this.light.position.set(-1, 2, 4);
     this.scene.add(this.light);
 
-    const objects3D = [
-      this.makeInstance(this.dodecGeometry, 0xff0000, false, 0, 0, 2),
-      this.makeInstance(this.planeGeometry, 0x0000ff, false, 0, 0, 0),
-    ];
+    const objects3D = [];
+
+    setInterval(() => {
+      objects3D.push(this.makeInstance(this.boxGeometry, '#' + Math.floor(Math.random() * 16777215).toString(16), false, Math.floor(Math.random() * 50), Math.floor(Math.random() * 50), Math.floor(Math.random() * 50)));
+    }, 500);
 
     objects3D.forEach(obj => {
       console.log('3D Object -> ', obj);
@@ -74,7 +75,7 @@ export class AppComponent implements OnInit {
 
     const animate = () => {
       objects3D.forEach(obj => {
-        if (obj.geometry.type == "DodecahedronBufferGeometry") {
+        if (obj.geometry.type == "BoxGeometry") {
           obj.rotation.x += .01;
         }
       });
